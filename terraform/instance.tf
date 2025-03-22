4ebbb6a781d9852cc472e1aa4f6a30491e30c084
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-03-22 19:52:29 -0400

    added terraform, docker

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,18 +1,51 @@
-
 resource "aws_instance" "node_docker" {
-  count                  = 5  # Creates 5 identical instances
-  ami                    = data.aws_ami.ubuntu_ami.id # Ubuntu amd64 (x86_64)
-  instance_type          = var.instance_type         # Free tier
+  count                  = 5
+  ami                    = data.aws_ami.ubuntu_ami.id
+  instance_type          = var.instance_type
   security_groups        = [aws_security_group.node_sg.name]
-  key_name               = "ssh_instance_key" # Please use your key name
- 
+  key_name               = "ssh_instance_key"
+
   root_block_device {
-    volume_size = 30 # Adjust size based on needs, in GB
+    volume_size = 30
     volume_type = "gp3"
   }
+
+  user_data = <<-EOF
+#!/bin/bash
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-compose-plugin
+# Enable services on reboot
+sudo systemctl enable docker
+    # Install Terraform
+sudo apt-get update -y
+sudo apt-get install -y gnupg software-properties-common wget
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+  | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update -y
+sudo apt-get install -y terraform
+EOF
+
   tags = {
-    Name = "NodeAppServer"
+    Name = "NodeAppServer-${count.index + 1}"
   }
 }
-
 
