25d3fcec183121104732b4331f5c52d2341f9d94
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-03-22 19:24:55 -0400

    added count 5

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,5 +1,6 @@
 
 resource "aws_instance" "node_docker" {
+  count                  = 5  # Creates 5 identical instances
   ami                    = data.aws_ami.ubuntu_ami.id # Ubuntu amd64 (x86_64)
   instance_type          = var.instance_type         # Free tier
   security_groups        = [aws_security_group.node_sg.name]
